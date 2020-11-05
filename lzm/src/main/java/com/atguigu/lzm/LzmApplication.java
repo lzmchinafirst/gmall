071e436734830a175c062f3bcd9a8ec34f9f0560
lzm <1098254010@qq.com>
2020-11-05 18:13:07 +0800

    dev commit

---
+++ lzm/src/main/java/com/atguigu/lzm/LzmApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.lzm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzmApplication.class, args);
+    }
+
+}
